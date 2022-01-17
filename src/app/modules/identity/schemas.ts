3578d441a7a355c0ceca81e4c84c5c74af1ab564
idntty <hello@idntty.org>
2022-01-17 20:22:00 +0300

    Some decorations

--- src/app/modules/identity/schemas.ts
+++ src/app/modules/identity/schemas.ts
@@ -1,16 +1,3 @@
-/*
-./bin/run config:create
-
-./bin/run genesis-block:create --output config/default --accounts 10 --validators-hash-onion-distance=10 --validators-passphrase-encryption-iterations=1000 --validators-hash-onion-count=1000
-tmp=$(mktemp)
-jq '.forging.delegates = input' config/default/config.json config/default/forging_info.json > "$tmp" && mv "$tmp" config/default/config.json
-jq '.forging += input' config/default/config.json config/default/password.json > "$tmp" && mv "$tmp" config/default/config.json
-
-./bin/run node:info
-
-*/
-
-
 const IdentityModuleSchema = {
     $id: 'identity/module',
     title: 'Identity module account schema',

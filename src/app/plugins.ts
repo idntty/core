2efcb8d871757cbf7d11abcf80e69a559216631d
idntty <hello@idntty.org>
2022-09-12 16:06:10 +0600

    Fix plugins

--- src/app/plugins.ts
+++ src/app/plugins.ts
@@ -1,5 +1,17 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Application } from 'lisk-sdk';
 
+import { IdnttyCoreApiPlugin } from "./plugins/idnttycoreapi";
+import { IdnttyTransactionHistoryPlugin } from "./plugins/idnttytxhistory";
+//import { IdnttyPrivateDataPlugin } from "./plugins/idnttyprivatedata";
+//import { IdnttyFaucetPlugin } from "./plugins/idnttyfaucet";
+
+
 // @ts-expect-error Unused variable error happens here until at least one module is registered
-export const registerPlugins = (_app: Application): void => {};
+export const registerPlugins = (app: Application): void => {
+    app.registerPlugin(IdnttyCoreApiPlugin);
+    app.registerPlugin(IdnttyTransactionHistoryPlugin);
+    
+    //app.registerPlugin(IdnttyPrivateDataPlugin);
+    //app.registerPlugin(IdnttyFaucetPlugin);
+};
